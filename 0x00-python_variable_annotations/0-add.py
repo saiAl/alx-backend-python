28df4908c9903d100c7121302ac07b1725ccf691
sai <ojiip95@gmail.com>
2024-08-01 09:19:32 +0200

    build: task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""0. Basic annotations - add"""
+import typing
+
+
+def add(a: float, b: float) -> float:
+    """
+      function that adds Two numbers
+        Args:
+          a (float): the 1st number
+          b (float): the 2nd number
+
+        Returns:
+          the sum of the Two numbers
+    """
+    return (a + b)
