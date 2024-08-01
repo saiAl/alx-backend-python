eb4ef2a255c9fb32e35c800f851ae7c2dcc856be
sai <ojiip95@gmail.com>
2024-08-01 18:21:12 +0200

    fix: the callback parameter

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -3,7 +3,7 @@
 from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable[float, float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Takes a float multiplier as argument and
         returns a function that multiplies a float by multiplier
     """
