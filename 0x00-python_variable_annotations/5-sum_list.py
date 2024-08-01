c0ee0c3c3e35a01b94884f48b56db3f4af5b5348
sai <ojiip95@gmail.com>
2024-08-01 17:53:53 +0200

    fix: type annotate the list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """5. Complex types - list of floats"""
-import typing
+from typing import List
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ "takes a list input_list of floats and return their sum"""
     res: float = 0
     for n in input_list:
