e766f88e5cc97e64fada4cb74ca22cc0fe7e5c61
sai <ojiip95@gmail.com>
2024-08-01 09:19:51 +0200

    build: task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""1. Basic annotations - concat"""
+import typing
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+      concat - function that concatenate two strings
+
+      Args:
+        str1 (str): the 1st string
+        str2 (str): the 2nd string
+
+      Returns:
+        new string
+    """
+    return f"{str1}{str2}"
