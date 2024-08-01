d004370d6ef98dc09ba1e6dcb9c9d53bb3c5cc4a
sai <ojiip95@gmail.com>
2024-08-01 09:20:21 +0200

    build: task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import typing
+"""3. Basic annotations - to string"""
+
+
+def to_str(n: float) -> str:
+    """that takes a float n as argument and returns the string"""
+    return str(n)
