3ee438c27ca273438f671722421c3410a2fc4edf
sai <ojiip95@gmail.com>
2024-08-01 17:56:32 +0200

    fix: type annotate the list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """6. Complex types - mixed list"""
-import typing
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: int) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ Takes list of integer and returns their sum as float"""
     res: int = 0
     for n in mxd_lst:
