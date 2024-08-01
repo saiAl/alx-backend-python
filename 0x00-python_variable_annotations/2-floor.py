ce636baff5dfb3516be6fae49b22298b0313dfb8
sai <ojiip95@gmail.com>
2024-08-01 09:20:03 +0200

    build: task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 2. Basic annotations - floor """
+import typing
+
+
+def floor(n: float) -> float:
+    """floor - function that return floor of n"""
+    return (n // 1)
