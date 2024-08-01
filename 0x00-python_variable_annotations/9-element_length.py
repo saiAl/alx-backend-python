5ac5baf17401828fb85ddd451ac95c0b899bd7e1
sai <ojiip95@gmail.com>
2024-08-01 18:28:53 +0200

    fix: fix the return value

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """9. Let's duck type an iterable object"""
-from typing import Iterable, Tuple, Sequence
+from typing import Iterable, Tuple, Sequence, List
 
 
-def element_length(lst: Iterable[Sequence]) -> list[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """ return the element length"""
     return [(i, len(i)) for i in lst]
