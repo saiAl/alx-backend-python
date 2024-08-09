15d7b76111b21a5c1485380a7e9f05143bb80cb5
sai <ojiip95@gmail.com>
2024-08-09 21:49:35 +0200

    fix: fix code style

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,10 +5,8 @@ import random
 import typing
 
 
-async def wait_random(max_delay: int =10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """ asynchronous coroutine """
     delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
-    return delay 
- 
-
+    return delay
