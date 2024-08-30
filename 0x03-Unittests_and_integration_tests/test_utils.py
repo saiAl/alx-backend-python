ea1bb95f479100407284de63db1dc233b0303b38
sai <ojiip95@gmail.com>
2024-08-30 17:52:35 +0200

    build: task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,7 +19,7 @@
 import unittest
 from unittest.mock import Mock, patch
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -105,6 +105,50 @@ class TestGetJson(unittest.TestCase):
 
         self.assertEqual(get_json(url), test_payload)
         mock.assert_called_once_with(url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Unit tests for the memoize decorator.
+
+        This test class verifies that the memoize decorator
+            correctly caches the result of a method,
+            ensuring the method is only executed once
+            even if accessed multiple times.
+    """
+    def test_memoize(self):
+        """Test that a method decorated with @memoize
+            is only called once, even if accessed multiple times.
+
+            A TestClass is defined with a method a_method
+                that returns a value. The a_property method is
+                decorated with @memoize and returns the result of a_method.
+            This test mocks a_method to ensure that it is only called
+                once when a_property is accessed multiple times.
+
+            Asserts:
+                The first and second access of a_property
+                    return the expected result (42).
+                a_method is only called once, as verified by
+                    mock_method.assert_called_once().
+        """
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        test_instance = TestClass()
+        with patch.object(
+                TestClass, 'a_method', return_value=42
+                ) as mock_method:
+            first_access = test_instance.a_property
+            second_access = test_instance.a_property
+
+            self.assertEqual(first_access, 42)
+            self.assertEqual(second_access, 42)
+            mock_method.asser_called_once()
 
 
 if __name__ == '__main__':
